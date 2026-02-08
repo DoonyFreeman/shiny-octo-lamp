66df4a60cfbcbbf11cdc5596f37cdaa00ce510ee
DoonyFreeman <rebrikov2006@gmail.com>
2026-02-09 01:21:49 +0300

    added Functional Program ex

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello Woooorld!!!")
+import math
+FIGURES = [
+    {'type': 'round', 'params': [3]},
+    {'type': 'rectangle', 'params': [3, 4]},
+    {'type': 'round', 'params': [9]},
+    {'type': 'rectangle', 'params': [4, 10]},
+]
+
+
+def get_square(figure):
+    if figure['type'] == 'round':
+        return math.pi * figure['params'][0] ** 2
+    elif figure['type'] == 'rectangle':
+        return figure['params'][0] * figure['params'][1]
+
+
+print(
+    sum(
+        map(
+            get_square,
+            FIGURES
+        )
+    )
+)
